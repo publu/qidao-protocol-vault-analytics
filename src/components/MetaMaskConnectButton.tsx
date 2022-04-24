4869e7db8d5ea75b3e1cd36fd9c10d719866237e
Matt Gottilla <matthew.gottilla@gmail.com>
2022-04-23 22:08:52 -0700

    Updated Button to display if we are connected or not

--- src/components/MetaMaskConnectButton.tsx
+++ src/components/MetaMaskConnectButton.tsx
@@ -2,19 +2,28 @@ import React, { useEffect } from "react"
 import { initializeConnector } from '@web3-react/core'
 import { MetaMask } from "@web3-react/metamask"
 
+const [metaMask, hooks] = initializeConnector<MetaMask>((actions) => new MetaMask(actions))
+const { useIsActive, useProvider, useChainId, useIsActivating } = hooks
 
 export const NetworkButton : React.FC<{title:string}> = ({children, title})=> {
-    const [metaMask, hooks] = initializeConnector<MetaMask>((actions) => new MetaMask(actions))
-    const { useIsActive, useProvider, useChainId, useIsActivating } = hooks
+    
     const active = useIsActive()
     const isActivating = useIsActivating()
     const provider = useProvider()
     const chainId = useChainId()
 
-    return (<button onClick={() => {
-        console.log("First log")
-        console.log(metaMask.activate())
-        console.log("Second log")}} title={title}>
+    useEffect(() => {
+        metaMask.connectEagerly()
+    }, [])
+
+    if (active === true) {
+        return (<button onClick={() => metaMask.activate()} title={title}>
+        <h3 style={{color: "white"}}>Connected</h3></button>)
+    }
+
+    console.log({active, isActivating, provider, chainId})
+
+    return (<button onClick={() => metaMask.activate()} title={title}>
         <h3 style={{color: "white"}}>Connect to Metamask</h3></button>)
     // Create a button with some text to act as a button, add it into the app, have the button prompt us to connect to metamask
 }
